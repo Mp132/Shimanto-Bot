8fc3c31912e9780688bcaf4f9e29dbfb22e77ac3
Mp132 <kdh030329@gmail.com>
2026-04-01 22:55:28 +0900

    feature=1

--- name.py
+++
@@ -1,7 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyB6JP4gBV5Y2S9dlavOzuHEyqV168MA0lE")
-
-for model in client.models.list():
-    # 모든 모델의 이름과 지원 기능을 출력해서 확인해보기
-    print(f"모델명: {model.name} / 지원기능: {model.supported_actions}")
